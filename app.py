76461f06ab75ed520c0a990beb9b4855ec093747
ThunderGer <Jaames11@hotmail.com>
2022-05-12 23:16:23 -0500

    guardando apikey

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from urllib import response
 import openai
 
-openai.api_key="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"
+openai.api_key= API_KEY;
 
 conversation = "Human: hello, who are you?\n AI: I am an IA created by OpenAI. How can I help you today?"
 
@@ -13,7 +12,7 @@ while(True):
     response = openai.Completion.create(
         engine="text-davinci-001",
         prompt=conversation,
-        temperature=1.0,
+        temperature=0.9,
         max_tokens=150,
         top_p=1.0,
         frequency_penalty=0.5,
